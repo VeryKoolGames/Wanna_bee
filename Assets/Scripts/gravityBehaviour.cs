81bf050515c325a19a8d6e8972c96dd16ebfa74a
Benjamin Vermot <vermotbenjamin@gmail.com>
2023-11-12 08:02:37 +0100

    ben updates

---
+++ Assets/Scripts/gravityBehaviour.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityBehaviour : MonoBehaviour
+{
+
+    //public GameObject planet;
+
+    //public planetScript planetScript;
+    //Rigidbody2D rb;
+
+    //bool isAttracted;
+
+
+
+
+
+    //private void Start()
+    //{
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.tag == "atmosphere")
+    //    {
+    //        planet = collision.gameObject;
+    //        planetScript = collision.GetComponent<planetScript>();
+    //        isAttracted = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "atmosphere")
+    //    {
+    //        isAttracted = false;
+    //    }
+    //}
+
+
+    //public void Update()
+    //{
+    //    if(isAttracted == true)
+    //    {
+    //        rb.AddForce((planet.transform.position - transform.position) * planetScript.gravityStrenght);
+    //    }
+
+    //    else
+    //    {
+    //        return;
+    //    }
+    //}
+
+
+  
+}
