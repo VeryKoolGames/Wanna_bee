4c56f334acf3316214704ff5baee17740771b02d
Benjamin Vermot <vermotbenjamin@gmail.com>
2023-11-14 21:09:20 +0100

    je test de commit mes trucs omgomgomgomg

---
+++ Assets/DespawnDelay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnDelay : MonoBehaviour
+{
+    [SerializeField] private float despawnDelay;
+    void Start()
+    {
+        StartCoroutine(Delay());
+    }
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(despawnDelay);
+        Destroy(this.gameObject);
+        
+    }
+
+    
+}
--- Assets/LerpHoney.cs
+++ Assets/LerpHoney.cs
@@ -8,9 +8,13 @@ public class LerpHoney : MonoBehaviour
     private bool startLerping;
     [SerializeField] private float speed = 2f;
     private GameObject player;
+    Vector3 smallHoneySize = new Vector3(0.2f, 0.2f, 0.2f);
+    [SerializeField] private GameObject trailParticles;
+
 
     private void Start()
     {
+        
         player = GameObject.FindWithTag("Player");
     }
 
@@ -27,6 +31,8 @@ public class LerpHoney : MonoBehaviour
         if (startLerping)
         {
             transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * speed);
+            transform.localScale = Vector3.Lerp(transform.localScale, smallHoneySize, Time.deltaTime * speed);
+            trailParticles.SetActive(true);
         }
     }
 }
